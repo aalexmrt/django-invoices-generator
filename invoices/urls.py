3974943a39f4cc4f4ce4bd7a441008065edf69cb
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-07-06 20:02:46 -0500

    update urls to change root path to view_invoices/

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
@@ -13,7 +14,7 @@ urlpatterns = [
     path('make_invoice/<int:id>/', views.make_invoice, name='make_invoice'),
     path('view_customers/', views.view_customers, name='view_customers'),
     path('view_invoices/', views.view_invoices, name='view_invoices'),
-    path('', views.index, name='index'),
+    path('', lambda req: redirect('/view_invoices/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
