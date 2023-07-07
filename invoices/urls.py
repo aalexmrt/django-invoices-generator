6217b0fee7c98ef4580c5805332b744743a3886c
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-07-06 19:03:52 -0500

    sort alphabetically the urls and some new views

--- invoices/urls.py
+++ invoices/urls.py
@@ -4,10 +4,15 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
+    path('add_customer/', views.add_customer, name='add_customer'),
     path('add_invoice/', views.add_invoice, name='add_invoice'),
-    path('make_invoice/<int:id>/', views.make_invoice, name='make_invoice'),
-    path('invoice_detail/<int:id>/', views.invoice_detail, name='invoice_detail'),
     path('check_pdf/<int:id>/', views.check_pdf, name='check_pdf'),
+    path('delete_invoice/<int:id>/', views.delete_invoice, name='delete_invoice'),
+    path('invoice_detail/<int:id>/', views.invoice_detail, name='invoice_detail'),
+    path('make_customer/<int:id>/', views.make_customer, name='make_customer'),
+    path('make_invoice/<int:id>/', views.make_invoice, name='make_invoice'),
+    path('view_customers/', views.view_customers, name='view_customers'),
+    path('view_invoices/', views.view_invoices, name='view_invoices'),
     path('', views.index, name='index'),
 ]
 
