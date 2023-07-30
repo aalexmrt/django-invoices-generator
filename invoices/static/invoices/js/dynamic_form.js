8cb0243bc8263257c8d489cb25a162788329362e
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-07-30 13:34:27 -0500

    refactor dynamic_form using jquery

--- invoices/static/invoices/js/dynamic_form.js
+++ invoices/static/invoices/js/dynamic_form.js
@@ -1,81 +1,82 @@
-// v.0.0.1
-
+// v.0.0.2
 // Starting program...
 
 function getParams(formsetName) {
-  let inputInitialFormset = document.querySelector(
-    `input[name="${formsetName}_set-INITIAL_FORMS"]`
-  ) // input#id_orderline_set-INITIAL_FORMS
+  // input#id_orderline_set-INITIAL_FORMS
+  let inputInitialFormset = $(`input[name="${formsetName}_set-INITIAL_FORMS"]`)
 
-  let inputTotalFormset = document.querySelector(
-    `input[name="${formsetName}_set-TOTAL_FORMS"]`
-  ) // input#id_orderline_set-TOTAL_FORMS
+  // input#id_orderline_set-TOTAL_FORMS
+  let inputTotalFormset = $(`input[name="${formsetName}_set-TOTAL_FORMS"]`)
 
   return [inputInitialFormset, inputTotalFormset]
 }
 
 function getRowId(formsetName, htmlDomElement) {
   let getIdRegex = (name) => {
-    return name.id.match(/\d+/) // ['0', index: 17, input: 'id_orderline_set-0-id', groups: undefined]
+    // ['0', index: 17, input: 'id_orderline_set-0-id', groups: undefined]
+    return name.id.match(/\d+/)
   }
 
   let rowId = getIdRegex(
-    htmlDomElement.querySelector(`input[id^="id_${formsetName}_set"]`) // input#id_orderline_set-0-id
+    // input#id_orderline_set-0-id
+    htmlDomElement.$(`input[id^="id_${formsetName}_set"]`)
   )[0] // '0'
 
   rowId = parseInt(rowId)
-
   return rowId
 }
 
 function insertAfterNode(previousNode, clonedNode) {
-  previousNode.parentNode.insertBefore(clonedNode, previousNode.nextSibling)
+  $(previousNode).after(clonedNode)
 }
+
 function addRow(htmlFormsetRow, button) {
-  let htmlClonedElement = htmlFormsetRow.cloneNode(true)
+  let htmlClonedElement = htmlFormsetRow.clone()
   let formsetRegexIds = `${formsetName}_set-\\d+` // orderline_set-1-id
   formsetRegexIds = new RegExp(formsetRegexIds, 'g')
 
-  inputTotalFormset.value = parseInt(inputTotalFormset.value) + 1
-  let newRowId = parseInt(inputTotalFormset.value) - 1
+  inputTotalFormset.value = parseInt(inputTotalFormset.val()) + 1
+  let newRowId = parseInt(inputTotalFormset.val()) - 1
   // Update the DOM element with the corresponding ids for the new row
-  htmlClonedElement.innerHTML = htmlClonedElement.innerHTML.replaceAll(
-    formsetRegexIds,
-    `${formsetName}_set-${newRowId}`
+  $(htmlClonedElement).html(
+    $(htmlClonedElement)
+      .html()
+      .replaceAll(formsetRegexIds, `${formsetName}_set-${newRowId}`)
   )
 
-  button.className = 'd-none'
+  button.addClass('d-none')
   // Insert the cloned element to the DOM
-  let newButton = htmlClonedElement.querySelector('.buttonDynamic')
+  let newButton = htmlClonedElement.$('.buttonDynamic')
   newButton = handleButton(newButton, 'addButton', htmlClonedElement)
   insertAfterNode(htmlFormsetRow, htmlClonedElement)
 }
 
 function handleButton(button, type, htmlFormsetRow) {
-  let clonedButton = button.cloneNode(true)
+  let clonedButton = button.clone()
 
   const addButton = {
     content: '+',
     style: 'btn btn-outline-success buttonDynamic',
   }
-  const removeButton = {
-    content: '-',
-    style: 'btn btn-outline-danger buttonDynamic',
-  }
+  //this functionality is not implemented yet
+  // const removeButton = {
+  //   content: '-',
+  //   style: 'btn btn-outline-danger buttonDynamic',
+  // }
 
   if (type === 'addButton') {
-    clonedButton.textContent = addButton.content
-    clonedButton.className = addButton.style
-    clonedButton.addEventListener('click', () =>
-      addRow(htmlFormsetRow, clonedButton)
-    )
-  } else if (type === 'removeButton') {
-    clonedButton.textContent = removeButton['content']
-    clonedButton.className = removeButton['style']
-    this.htmlButton.addEventListener('click', () => removeRow())
+    clonedButton.text(addButton.content)
+    clonedButton.addClass(addButton.style)
+    clonedButton.click(() => addRow(htmlFormsetRow, clonedButton))
+    // this functionality is not implemented yet
+    // } else if (type === 'removeButton') {
+    //   clonedButton.textContent = removeButton['content']
+    //   clonedButton.className = removeButton['style']
+    //   this.htmlButton.addEventListener('click', () => removeRow())
+    // }
   } else {
-    clonedButton.textContent = ''
-    clonedButton.className = 'd-none'
+    clonedButton.text('')
+    clonedButton.addClass('d-none')
   }
 
   button.replaceWith(clonedButton)
@@ -83,16 +84,13 @@ function handleButton(button, type, htmlFormsetRow) {
   return clonedButton
 }
 
-const htmlFormsetDynamicRows = document.querySelectorAll('.formsetDynamic')
-
+const htmlFormsetDynamicRows = document.$('.formsetDynamic')
 const formsetName = 'orderline'
-
 let [inputInitialFormset, inputTotalFormset] = getParams(formsetName)
 
-htmlFormsetDynamicRows.forEach((htmlFormsetRow) => {
+htmlFormsetDynamicRows.each((htmlFormsetRow) => {
   let rowId = getRowId(formsetName, htmlFormsetRow)
-  console.log(rowId)
-  let button = htmlFormsetRow.querySelector('.buttonDynamic')
+  let button = htmlFormsetRow.$('.buttonDynamic')
 
   if (rowId === htmlFormsetDynamicRows.length - 1) {
     button = handleButton(button, 'addButton', htmlFormsetRow)
