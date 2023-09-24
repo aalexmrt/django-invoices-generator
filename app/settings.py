e2c65dcaaed954cfc09b9486a71bc6daab1f8e31
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-09-24 18:05:24 -0500

    trying to fix deployment problems

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = []
