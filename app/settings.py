493c37b58a2e02077b06b70075d6d870ad26f809
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-09-24 13:12:06 -0500

    add import os to settings.py

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
