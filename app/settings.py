feba3a75f34557920ef9beeb291c53841acb64d6
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-09-24 13:10:46 -0500

    struggling deploying to vercel

--- app/settings.py
+++ app/settings.py
@@ -136,6 +136,9 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 MEDIA_ROOT = 'media/'
 
 # Default primary key field type
